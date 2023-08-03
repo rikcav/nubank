7e6acaa4277faef2085ee8e2002ed64fff2bcd9f
rikcav <henriquearau000@gmail.com>
2023-08-03 13:28:01 -0300

    feat: adicionado componente de Header

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function Header() {
+  return (
+      <View style={styles.container}>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#8303D2',
+    width: '100%',
+    height: '20%',
+    alignSelf: 'flex-start',
+  },
+});
