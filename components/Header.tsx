d5d33349e6db36ce11d384745dfc837205f45904
rikcav <henriquearau000@gmail.com>
2023-08-03 13:58:59 -0300

    refactor: pequena mudança na escrita do componente Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,7 @@
 import { StyleSheet, View } from 'react-native';
 
 export default function Header() {
-  return (
-      <View style={styles.container}>
-      </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
