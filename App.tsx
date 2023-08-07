40ad3ccc74b2ad2bdc8866474f08435cc725b156
rikcav <henriquearau000@gmail.com>
2023-08-07 16:51:19 -0300

    chore: modificações para testar componente de botão

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
+import RowButton from './components/RowButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <Header />
+      <RowButton bodyText="Pix" imageURL={"pix"} />
     </View>
   );
 }
